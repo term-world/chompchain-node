d506cd216e3959bcad587376aa625e5be8cd135c
dluman <dluman@allegheny>
2023-05-27 15:19:43 -0400

    Fixing hash comparison; Python JSON adds spaces

--- node/src/index.js
+++ node/src/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra');
 const http = require('http');
 const express = require('express');
-const env = require('dotenv').config({ path: '/opt/server/.env' });
+const env = require('dotenv').config(); // ({ path: '/opt/server/.env' });
+const { createHash } = require('crypto');
 
 let server = express();
 server.use(express.json());
@@ -28,18 +29,28 @@ const assign = async () => {
     return random;
 }
 
-const verified = async () => {
-    return true;
+const verified = async (txn) => {
+    // Save hash for comparison
+    let txHash = txn.hash;
+    // Remove extraneous unhashed fields
+    delete txn.hash;
+    delete txn.timestamp;
+    // Make the hash from remainder
+    let hashable = JSON.stringify(txn);
+    let checksum = createHash('sha256').update(hashable).digest('hex');
+    return checksum == txHash;
 }
 
 server.post("/transact", async (req, res) => {
     let record = req.body.txn;
     let filename = `${await assign()}.json`;
     if(await verified(req.body.txn)){
-        fs.writeFile(`${process.env.MEMPOOL}{filename}`, JSON.stringify(req.body.txn), (err) => {
-            console.log(err);
-            res.sendStatus(500);
-            return;
+        fs.writeFile(`${process.env.MEMPOOL}/{filename}`, JSON.stringify(req.body.txn), (err) => {
+            if(err) {
+                console.log(`500: ${err}`);
+                res.sendStatus(500);
+                return;
+            }
         });
         res.sendStatus(200);
         return;
