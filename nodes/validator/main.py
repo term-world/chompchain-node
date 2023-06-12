a5104e3840ea9fcd0de3d3067740d04e7d1fd9af
dluman <dluman@allegheny.edu>
2023-06-12 12:50:33 -0400

    Reducing instruction set for Python translation of function

--- nodes/validator/main.py
+++ nodes/validator/main.py
@@ -4,34 +4,15 @@ import aiohttp
 import asyncio
 from aiohttp import web
 
-async def register_node():
-    url = "https://dir.chain.chompe.rs/register"
-    data = {"port": 5000}
+async def transaction_new(request):
+    try:
+        txn = await request.json()
+        if await is_valid(txn):
+            pass
+        return web.Response(status = 200)
+    except:
+        return web.Response(status = 500)
 
-    async with aiohttp.ClientSession() as session:
-        while True:
-            try:
-                async with session.post(url, json=data) as response:
-                    if response.status == 200:
-                        print("Registered...")
-                        break
-            except aiohttp.ClientError:
-                print("Unable to register node...")
-                return
-
-            await asyncio.sleep(5)
-
-
-# async def transaction_new(request):
-#     try:
-#         request_body = await request.json()
-#         print(request_body)
-#         if not "name" in request_body:
-#             return web.Response(status = 418)
-#         return web.Response(status = 200)
-#     except:
-#         return web.Response(status = 500)
-    
 # async def mempool(request):
 #     mempool_dir = os.environ.get('MEMPOOL')
 #     files = await asyncio.get_event_loop().run_in_executor(None, os.listdir, mempool_dir)
@@ -50,31 +31,26 @@ async def register_node():
 #     return str(random_number)
     
 
-
-
-# async def verify(txn):
-#     tx_hash = txn['hash']
-#     # Create a new dictionary without 'hash' and 'signature' keys
-#     hashables = {}
-#     for k, v in txn.items():
-#         if k != 'hash' and k != 'signature':
-#             hashables[k] = v
-#     # Sort the keys in alphabetical order
-#     sorted_keys = sorted(hashables.keys())
-#     # Convert the sorted keys and corresponding values to a string
-#     hashable_str = "{"
-#     for key in sorted_keys:
-#         value = hashables[key]
-#         hashable_str += f"'{key}': {json.dumps(value)}, "
-#     hashable_str = hashable_str.rstrip(', ') + "}"
-#     # Convert the string to bytes using UTF-8 encoding
-#     hashable_bytes = hashable_str.encode('utf-8')
-#     # Calculate the SHA256 hash of the bytes
-#     sha256_hash = hashlib.sha256()
-#     sha256_hash.update(hashable_bytes)
-#     checksum = sha256_hash.hexdigest()
-#     # Check if the calculated checksum matches the original tx_hash
-#     return checksum == tx_hash
+async def is_valid(txn: dict = {}) -> bool:
+    hash = txn['hash']
+    # Create a new dictionary without 'hash' and 'signature' keys
+    hashables = {}
+    for key, val in txn.items():
+        if not key in ["hash", "timestamp", "signature"]:
+            hashables[key] = val
+    # Dump string minified (i.e. no spaces around separators)
+    hashable_str = json.dumps(
+        hashables,
+        separators = (",",":")
+    )
+    # Encode string as bytestring
+    hashable_bytes = hashable_str.encode('utf-8')
+    # Calculate the SHA256 hash of the bytes
+    sha256_hash = hashlib.sha256()
+    sha256_hash.update(hashable_bytes)
+    checksum = sha256_hash.hexdigest()
+    # Return result of hex string comparison
+    return checksum == hash
 
 
 # aiohttp application setup
@@ -84,6 +60,4 @@ app = web.Application()
 app.add_routes(
     [web.post("/transactions/new", transaction_new)]
 )
-app.add_routes(web.get('/', handle)),
-web.run_app(app)
-
+web.run_app(app, port = 7500)
--- testing/test.py
+++ testing/test.py
@@ -1,26 +1,12 @@
 import requests
 import json
-# from chompchainwallet.interface import Transaction
-# transaction = Transaction(
-#     wallet=Wallet(),
-#     to_addr="...",
-#     data={...},
-#     from_addr="..."
-# )
-# transaction_data = {
-#     'data': transaction.data,
-#     'to_addr': transaction.to_addr,
-#     'from_addr': transaction.from_addr,
-#     'hash': transaction.hash,
-#     'timestamp': transaction.timestamp,
-#     'signature': transaction.signature
-# }
 
-data = {'msg': 'hi', 'name': 'Professor'}
+with open("data.json", "r") as fh:
+    data = json.load(fh)
 
 response = requests.post(
     # URL -> "endpoint"
-    "http://localhost:8080/transactions/new",
+    "http://localhost:7500/transactions/new",
     data = json.dumps(data)
 )
 
