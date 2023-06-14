001cdc2ee1a3e30dd4e010b00cd277c56eb9212f
dluman <dluman@allegheny.edu>
2023-06-13 21:07:26 -0400

    Updating with consolidation; new transaction validity reference

--- nodes/validator/main.py
+++ nodes/validator/main.py
@@ -1,22 +1,39 @@
+import os
 import json
+import random
+import secrets
 import hashlib
 import aiohttp
 import asyncio
+
 from aiohttp import web
-import os 
-import random
 
 async def transaction_new(request):
     try:
         txn = await request.json()
-        if await is_valid(txn):
-            pass
+        if await is_valid_hash(txn) and await is_valid_txn(txn):
+            filename = await assign_filename()
+            file_path = f"{os.environ['MEMPOOL']}/{filename}.json"
+            with open(file_path, 'w') as file:
+                json.dump(txn, file)
         return web.Response(status = 200)
     except:
         return web.Response(status = 500)
+    # If we reach this point, the request is
+    # at fault.
+    return web.Response(status = 418)
 
+async def is_valid_txn(txn: dict = {}) -> bool:
+    keys = ["data", "to_addr", "from_addr",
+            "hash", "timestamp", "signature"]
+    for key in keys:
+        if key not in txn:
+            return False
+        if len(keys[key]) < 1:
+            return False
+    return True
 
-async def is_valid(txn: dict = {}) -> bool:
+async def is_valid_hash(txn: dict = {}) -> bool:
     hash = txn['hash']
     # Create a new dictionary without 'hash' and 'signature' keys
     hashables = {}
@@ -37,41 +54,14 @@ async def is_valid(txn: dict = {}) -> bool:
     # Return result of hex string comparison
     return checksum == hash
 
-
-async def transact_handler(request):
-    try:
-        data = await request.json()
-        key = data.get('key')
-        txn = data.get('txn')
-        sig = data.get('txn', {}).get('signature')
-
-        filename = await assign()
-
-        if await is_valid(txn) and await signed(sig, key):
-            file_path = f"{os.environ['MEMPOOL']}/{filename}.json"
-            with open(file_path, 'w') as file:
-                json.dump(txn, file)
-            return web.Response(status=200)
-        
-        return web.Response(status=418)  # I'M A TEAPOT
-    except:
-        return web.Response(status=500)
-
-async def assign():
-    max_value = 10000
-    min_value = 1000
+async def assign_filename():
     files = os.listdir(os.environ['MEMPOOL'])
-    random_number = random.randint(min_value, max_value)
-    
-    while f"{random_number}.json" in files:
-        random_number = random.randint(min_value, max_value)
-    
-    return random_number
+    filename = secrets.token_hex(3)
 
-async def signed(sig, key):
-    # Implementation for signature verification goes here
-    pass
+    while f"{filename}.json" in files:
+        filename = secrets.token_hex(3)
 
+    return filename
 
 # aiohttp application setup
 app = web.Application()
